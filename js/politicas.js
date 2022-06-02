86115ee8e70f47046f4c2a42c35ce1c6d6b68237
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-01 19:07:19 -0500

    se agrega politicas.js el cual contienes las descripciones de las politicas de privacidad y de cookies de la pagina.

---
+++ js/politicas.js
@@ -0,0 +1,147 @@
+const politicaPrivacidadDescripcion = `
+<strong>Política de privacidad de Medsan Security</strong>
+<br>
+<p>
+Esta Política de privacidad describe cómo se recopila, utiliza y comparte su información
+personal cuando visita o hace una compra en www.medsansecurity.com (denominado en lo
+sucesivo el “Sitio”).
+INFORMACIÓN PERSONAL QUE RECOPILAMOS
+Cuando visita el Sitio, recopilamos automáticamente cierta información sobre su dispositivo,
+incluida información sobre su navegador web, dirección IP, zona horaria y algunas de las
+cookies que están instaladas en su dispositivo. Además, a medida que navega por el Sitio,
+recopilamos información sobre las páginas web individuales o los productos que ve, las
+páginas web o los términos de búsqueda que lo remitieron al Sitio e información sobre cómo
+interactúa usted con el Sitio. Nos referimos a esta información recopilada automáticamente
+como “Información del dispositivo”.
+Recopilamos Información del dispositivo mediante el uso de las siguientes tecnologías:
+- Los “Archivos de registro” rastrean las acciones que ocurren en el Sitio y recopilan
+datos, incluyendo su dirección IP, tipo de navegador, proveedor de servicio de Internet,
+páginas de referencia/salida y marcas de fecha/horario.
+- Las “balizas web”, las “etiquetas” y los “píxeles” son archivos electrónicos utilizados
+para registrar información sobre cómo navega usted por el Sitio.
+[[INSERTAR DESCRIPCIONES DE OTROS TIPOS DE TECNOLOGÍAS DE SEGUIMIENTO QUE SE UTILICEN]]
+Además, cuando hace una compra o intenta hacer una compra a través del Sitio, recopilamos
+cierta información de usted, entre la que se incluye su nombre, dirección de facturación,
+dirección de envío, información de pago (incluidos los números de la tarjeta de crédito
+[[INSERTAR CUALQUIER OTRO TIPO DE PAGO ACEPTADO]]), dirección de correo electrónico y número
+de teléfono. Nos referimos a esta información como “Información del pedido”.
+[[INSERTAR CUALQUIER OTRA INFORMACIÓN QUE USTED RECOPILA: DATOS SIN CONEXIÓN, LISTAS/DATOS
+DE MARKETING ADQUIRIDOS]]
+Cuando hablamos de “Información personal” en la presente Política de privacidad, nos
+referimos tanto a la Información del dispositivo como a la Información del pedido.
+¿CÓMO UTILIZAMOS SU INFORMACIÓN PERSONAL?
+Usamos la Información del pedido que recopilamos en general para preparar los pedidos
+realizados a través del Sitio (incluido el procesamiento de su información de pago, la
+organización de los envíos y la entrega de facturas y/o confirmaciones de pedido). Además,
+utilizamos esta Información del pedido para: comunicarnos con usted;
+examinar nuestros pedidos en busca de fraudes o riesgos potenciales; y cuando de acuerdo con
+las preferencias que usted compartió con nosotros, le proporcionamos información o
+publicidad relacionada con nuestros productos o servicios.
+[[INSERTAR OTROS USOS DE INFORMACIÓN DEL PEDIDO]]
+Utilizamos la Información del dispositivo que recopilamos para ayudarnos a detectar posibles
+riesgos y fraudes (en particular, su dirección IP) y, en general, para mejorar y optimizar
+nuestro Sitio (por ejemplo, al generar informes y estadísticas sobre cómo nuestros clientes
+navegan e interactúan con el Sitio y para evaluar el éxito de nuestras campañas
+publicitarias y de marketing).
+[[INSERTAR OTROS USOS DE INFORMACIÓN DEL DISPOSITIVO, INCLUIDOS PUBLICIDAD/RETARGETING]]
+COMPARTIR SU INFORMACIÓN PERSONAL
+Compartimos su Información personal con terceros para que nos ayuden a utilizar su
+Información personal, tal como se describió anteriormente. Por ejemplo, utilizamos la
+tecnología de Shopify en nuestra tienda online. Puede obtener más información sobre cómo
+Shopify utiliza su Información personal aquí: https://www.shopify.com/legal/privacy. También
+utilizamos Google Analytics para ayudarnos a comprender cómo usan nuestros clientes el
+Sitio. Puede obtener más información sobre cómo Google utiliza su Información personal aquí:
+https://www.google.com/intl/es/policies/privacy/. Puede darse de baja de Google Analytics
+aquí: https://tools.google.com/dlpage/gaoptout.
+Finalmente, también podemos compartir su Información personal para cumplir con las leyes y
+regulaciones aplicables, para responder a una citación, orden de registro u otra solicitud
+legal de información que recibamos, o para proteger nuestros derechos.
+[[INCLUIR SI SE USA REMARKETING O PUBLICIDAD DIRIGIDA]]
+PUBLICIDAD ORIENTADA POR EL COMPORTAMIENTO
+Como se describió anteriormente, utilizamos su Información personal para proporcionarle
+anuncios publicitarios dirigidos o comunicaciones de marketing que creemos que pueden ser de
+su interés. Para más información sobre cómo funciona la publicidad dirigida, puede visitar
+la página educativa de la Iniciativa Publicitaria en la Red ("NAI" por sus siglas en inglés)
+en http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
+Puede darse de baja de la publicidad dirigida mediante los siguientes enlaces:
+[[
+INCLUIR ENLACES PARA DARSE DE BAJA DE CUALQUIER SERVICIO QUE SE UTILICE.
+ALGUNOS ENLACES FRECUENTES INCLUYEN:
+FACEBOOK: https://www.facebook.com/settings/?tab=ads
+GOOGLE: https://adssettings.google.com/authenticated?hl=es
+BING: https://about.ads.microsoft.com/es-es/recursos/directivas/anuncios-personalizados
+]]
+Además, puede darse de baja de algunos de estos servicios visitando el portal de exclusión
+voluntaria de Digital Advertising Alliance en: ttp://optout.aboutads.info/.
+NO RASTREAR
+Tenga en cuenta que no alteramos las prácticas de recopilación y uso de datos de nuestro
+Sitio cuando vemos una señal de No rastrear desde su navegador.
+[[INCLUIR SI LA TIENDA ESTÁ UBICADA EN EUROPA O SI TIENE CLIENTES EN EUROPA]]
+SUS DERECHOS
+Si usted es un residente europeo, tiene derecho a acceder a la información personal que
+tenemos sobre usted y a solicitar que su información personal sea corregida, actualizada o
+eliminada. Si desea ejercer este derecho, comuníquese con nosotros a través de la
+información de contacto que se encuentra a continuación.
+Además, si usted es un residente europeo, tenemos en cuenta que estamos procesando su
+información para cumplir con los contratos que podamos tener con usted (por ejemplo, si
+realiza un pedido a través del Sitio) o para perseguir nuestros intereses comerciales
+legítimos enumerados anteriormente. Además, tenga en cuenta que su información se
+transferirá fuera de Europa, incluidos Canadá y los Estados Unidos.
+RETENCIÓN DE DATOS
+Cuando realiza un pedido a través del Sitio, mantendremos su Información del pedido para
+nuestros registros a menos que y hasta que nos pida que eliminemos esta información.
+[[INSERTAR SI SE REQUIERE RESTRICCIÓN DE EDAD]]
+MENORES
+El sitio no está destinado a personas menores de [[INSERTAR EDAD]].
+CAMBIOS
+Podemos actualizar esta política de privacidad periódicamente para reflejar, por ejemplo,
+cambios en nuestras prácticas o por otros motivos operativos, legales o reglamentarios.
+CONTÁCTENOS
+Para obtener más información sobre nuestras prácticas de privacidad, si tiene alguna
+pregunta o si desea presentar una queja, contáctenos por correo electrónico a
+jsbustos43@misena.edu.co o por correo mediante el uso de la información que se proporciona a
+continuación:
+Kra 92 #8c-36, Castilla, Kennedy, Bogotá D.C., Bogota, DC, 11166, Colombia </p>`;
+
+
+const politicaCookiesDescripcion = `
+<p> El acceso a este Sitio Web puede implicar la utilización de cookies. Las cookies son
+pequeñas cantidades de información que se almacenan en el navegador utilizado por cada
+Usuario —en los distintos dispositivos que pueda utilizar para navegar— para que el servidor
+recuerde cierta información que posteriormente y únicamente el servidor que la implementó
+leerá. Las cookies facilitan la navegación, la hacen más amigable, y no dañan el dispositivo
+de navegación.
+Las cookies son procedimientos automáticos de recogida de información relativa a las
+preferencias determinadas por el Usuario durante su visita al Sitio Web con el fin de
+reconocerlo como Usuario, y personalizar su experiencia y el uso del Sitio Web, y pueden
+también, por ejemplo, ayudar a identificar y resolver errores.
+La información recabada a través de las cookies puede incluir la fecha y hora de visitas al
+Sitio Web, las páginas visionadas, el tiempo que ha estado en el Sitio Web y los sitios
+visitados justo antes y después del mismo. Sin embargo, ninguna cookie permite que esta
+misma pueda contactarse con el número de teléfono del Usuario o con cualquier otro medio de
+contacto personal. Ninguna cookie puede extraer información del disco duro del Usuario o
+robar información personal. La única manera de que la información privada del Usuario forme
+parte del archivo Cookie es que el usuario dé personalmente esa información al servidor.
+Las cookies que permiten identificar a una persona se consideran datos personales. Por
+tanto, a las mismas les será de aplicación la Política de Privacidad anteriormente descrita.
+En este sentido, para la utilización de las mismas será necesario el consentimiento del
+Usuario. Este consentimiento será comunicado, en base a una elección auténtica, ofrecido
+mediante una decisión afirmativa y positiva, antes del tratamiento inicial, removible y
+documentado.
+Deshabilitar, rechazar y eliminar cookies
+El Usuario puede deshabilitar, rechazar y eliminar las cookies —total o parcialmente—
+instaladas en su dispositivo mediante la configuración de su navegador (entre los que se
+encuentran, por ejemplo, Chrome, Firefox, Safari, Explorer). En este sentido, los
+procedimientos para rechazar y eliminar las cookies pueden diferir de un navegador de
+Internet a otro. En consecuencia, el Usuario debe acudir a las instrucciones facilitadas por
+el propio navegador de Internet que esté utilizando. En el supuesto de que rechace el uso de
+cookies —total o parcialmente— podrá seguir usando el Sitio Web, si bien podrá tener
+limitada la utilización de algunas de las prestaciones del mismo.</p>`;
+
+
+// show descriptions in the index
+$('#politica-privacidad-descripcion').html(politicaPrivacidadDescripcion);
+$('#politica-cookies-descripcion').html(politicaCookiesDescripcion);
+
+
+
